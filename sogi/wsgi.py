f54ff6b9d1e9a277709d13740a12dc413eb82e72
william1lai <williamlai@ucla.edu>
2015-12-15 12:32:53 -0800

    trying to get to work for heroku try 7

--- sogi/wsgi.py
+++ sogi/wsgi.py
@@ -6,8 +6,8 @@ from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 from django.conf import settings
 
-application = Cling(get_wsgi_application())
-
 if not settings.configured:
     settings.configure()
     
+application = Cling(get_wsgi_application())
+
