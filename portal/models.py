4bee83f0f19d8dff22a373ccb36bd4ea19f3c407
william1lai <williamlai@ucla.edu>
2015-12-17 18:43:43 -0800

    change models to fit new schema

--- portal/models.py
+++ portal/models.py
@@ -14,27 +14,19 @@ class Song(models.Model):
     url = models.URLField()
     time_added = models.DateTimeField(auto_now_add=True)
     
+    rank = models.PositiveIntegerField()
+    rating = models.PositiveIntegerField()
+    comment = models.CharField(max_length=2048)
+    turn_time = models.DateTimeField()
+    
 class Turn(models.Model):
     owner = models.ForeignKey(Member)
-    song_list = models.ManyToManyField(
-        Song,
-        through='Evaluation',
-        through_fields=('turn', 'song'),
-    )
-    turn_time = models.DateTimeField(auto_now_add=True)
+    song_list = models.ManyToManyField(Song)
     
 class Group(models.Model):
     member_list = models.ManyToManyField(Member)
     name = models.CharField(max_length=128)
     password = models.CharField(max_length=128)
     turn = models.IntegerField()
-    history = models.ManyToManyField(Turn)
-    
-class Evaluation(models.Model):
-    turn = models.ForeignKey(Turn, on_delete=models.CASCADE)
-    song = models.ForeignKey(Song, on_delete=models.CASCADE)
-    rank = models.PositiveIntegerField()
-    rating = models.PositiveIntegerField()
-    comment = models.CharField(max_length=1024)
-    
+    prev_turn = models.ForeignKey(Turn)    
     
