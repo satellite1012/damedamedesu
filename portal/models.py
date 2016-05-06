975b346d7a5d631f89c718a225897df7268a007d
william1lai <williamlai@ucla.edu>
2016-05-06 10:14:01 -0700

    trying to fix model problems

--- portal/models.py
+++ portal/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Game(models.Model):
-    day = models.DateField
+    day = models.DateField()
+    win = models.IntegerField()
     champs = models.CharField(max_length=2048) #encoded as name1/name2/name3/name4/name5
-    win = models.IntegerField
     
