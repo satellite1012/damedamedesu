4e7e499d09b5c4b9ff463aab4e42fee9d96df399
william1lai <williamlai@ucla.edu>
2016-05-10 09:26:47 -0700

    sorting list

--- ddd/views.py
+++ ddd/views.py
@@ -19,9 +19,9 @@ def data_page(request):
     cdata = {}
     for key in champlist:
         cdata[key] = data.filter(champs__contains=key).count()
-    #sorteddata = sorted(cdata)#, key=lambda item: item[1], reverse=True)
+    sorteddata = sorted(cdata, key=lambda item: item[1], reverse=True)
  
-    return render(request, 'data.html', {'d': cdata},
+    return render(request, 'data.html', {'d': sorteddata},
         context_instance=RequestContext(request))
 
 def logout_page(request):
