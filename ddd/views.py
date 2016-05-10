eb97beeb65314c8e466b72f3b86712292d8b2ad0
william1lai <williamlai@ucla.edu>
2016-05-10 08:54:08 -0700

    debug7

--- ddd/views.py
+++ ddd/views.py
@@ -5,7 +5,9 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.template import RequestContext
 from portal.models import Game
-
+    
+champlist = [ "Annie", "Yasuo", "Zed" ]
+    
 def main_page(request):
     if request.user.is_authenticated():
         return HttpResponseRedirect('/portal/')
@@ -13,14 +15,13 @@ def main_page(request):
         context_instance=RequestContext(request))
         
 def data_page(request):
-    #champlist = [ "Annie", "Yasuo", "Zed" ]
     data = Game.objects.all()
-    #cdata = {}
-#    for key in champlist:
-#        cdata[key] = data.filter(champs__contains=key).count()
-    #sorteddata = sorted(cdata)#, key=lambda item: item[1], reverse=True)
+    cdata = {}
+    for key in champlist:
+        cdata[key] = data.filter(champs__contains=key).count()
+    sorteddata = sorted(cdata)#, key=lambda item: item[1], reverse=True)
     
-    return render(request, 'data.html', {'d': data},
+    return render(request, 'data.html', {'d': sorteddata},
         context_instance=RequestContext(request))
 
 def logout_page(request):
